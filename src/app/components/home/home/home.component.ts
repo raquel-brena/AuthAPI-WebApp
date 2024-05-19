3232af009431bd643e4e08f6c7e96de0097647d0
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-19 03:41:51 -0300

    feat: Add home component with menu and content sections

---
+++ src/app/components/home/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
+import { ConteudoComponent } from '../conteudo/conteudo.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MenuComponent, ConteudoComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss',
+})
+export class HomeComponent {}
