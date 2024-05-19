5e3e97f4d113754a3497d51cf51ee1285254f6d1
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-19 03:41:24 -0300

    feat: Add menu component with buttons for different categories

---
+++ src/app/components/home/menu/menu.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ItemMenuComponent } from '../item-menu/item-menu.component';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [ItemMenuComponent],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.scss'
+})
+export class MenuComponent {
+
+}
