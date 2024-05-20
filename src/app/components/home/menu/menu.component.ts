dbd886aba853c8c1cf102d3687e209b9694eb949
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-20 01:21:33 -0300

    refactor: Update menu component to use dynamic item list and emit selected item

--- src/app/components/home/menu/menu.component.ts
+++ src/app/components/home/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { ItemMenuComponent } from '../item-menu/item-menu.component';
 
 @Component({
@@ -6,8 +6,25 @@ import { ItemMenuComponent } from '../item-menu/item-menu.component';
   standalone: true,
   imports: [ItemMenuComponent],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.scss'
+  styleUrl: './menu.component.scss',
 })
 export class MenuComponent {
+  @Output() itemMenuSelected = new EventEmitter<string>();
 
+  items = [
+    { category: 'Acessorio', image_url: 'assets/acessorio.svg' },
+    { category: 'Notebook', image_url: 'assets/notebook.svg' },
+    { category: 'Desktop', image_url: 'assets/desktop.svg' },
+    { category: 'Monitor', image_url: 'assets/monitor.svg' },
+    { category: 'Cadeira', image_url: 'assets/cadeira.svg' },
+  ];
+  
+  selectedItem: string = 'Acessorio';
+
+  constructor() {}
+
+  changeItemMenuSelected(item: string) {
+    this.selectedItem = item;
+    this.itemMenuSelected.emit(item);
+  }
 }
