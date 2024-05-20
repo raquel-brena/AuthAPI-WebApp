05b44eea520679e40d0bf6a965cd8fd5d581df76
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-20 00:20:31 -0300

    chore: Update app.config.ts with HttpClient and HttpClientModule providers

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { importProvidersFrom } from '@angular/core';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+  ],
 };
