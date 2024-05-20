724b8583a99b4c124963407c0d83cb029b213e9e
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-20 00:21:56 -0300

    feat: Add category input to card component

--- src/app/components/home/card/card.component.ts
+++ src/app/components/home/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../../../models/product';
 
 @Component({
   selector: 'app-card',
@@ -17,11 +18,31 @@ export class CardComponent {
   @Input()
   sku!: string;
   @Input() quantity!: number;
+  @Input() category!: string;
   @Input() isAddSelected!: boolean;
   @Output() isAddSelectedChange = new EventEmitter<boolean>();
+  @Output() productSelected = new EventEmitter<Product>();
 
-  select = () => {
+  select = (
+    sku: string,
+    name: string ,
+    description: string | undefined,
+    quantity: number,
+    price: number,
+    category: string
+  ) => {
+
+    const product: Product = {
+      sku,
+      name,
+      description: description || '',
+      quantity,
+      price,
+      category
+    };
+
     this.isAddSelected = !this.isAddSelected;
     this.isAddSelectedChange.emit(this.isAddSelected);
+    this.productSelected.emit(product);
   };
 }
