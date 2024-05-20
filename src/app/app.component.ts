e63c799349e43dfc2341949b551ac1a2d1164b01
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-20 00:20:23 -0300

    feat: Update app component with lazy loading for menu, barra-superior, and conteudo components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/home/menu/menu.component';
+import { BarraSuperiorComponent } from './components/home/barra-superior/barra-superior.component';
+import { ConteudoComponent } from './components/home/conteudo/conteudo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'auth-api';
