5062b59a4fc2a55d21a780e220cd88acbb08cc1f
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-20 00:20:37 -0300

    feat: Add routes for home, login, and register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/form/login/login.component';
+import { HomeComponent } from './components/home/home/home.component';
+import { RegisterComponent } from './components/form/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '**', redirectTo: 'login' },
+];
