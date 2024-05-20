e7a4ccea0af1ec0f2447acdf873966d2576b0470
Raquel <raquel.lima.124@ufrn.edu.br>
2024-05-20 00:21:38 -0300

    chore: Update container-card component with new button and styles

--- src/app/components/home/container-card/container-card.component.ts
+++ src/app/components/home/container-card/container-card.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CardComponent } from '../card/card.component';
 import { Product } from '../../../models/product';
 import { ProductService } from '../../../services/product.service';
+import { AddProductComponent } from '../../form/add-product/add-product.component';
 @Component({
   selector: 'app-container-card',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, AddProductComponent],
   templateUrl: './container-card.component.html',
   styleUrl: './container-card.component.scss',
 })
@@ -13,8 +14,18 @@ export class ContainerCardComponent implements OnInit {
   constructor(private productService: ProductService) {}
 
   products: Product[] = [];
-  isAddSelected: boolean = false;
 
+  productSelected: Product = {
+    name: '',
+    price: 0,
+    description: '',
+    sku: '',
+    quantity: 0,
+    category: '',
+  };
+
+  isAddSelected: boolean = false;
+  
   ngOnInit() {
     this.fetchProducts();
   }
@@ -23,5 +34,10 @@ export class ContainerCardComponent implements OnInit {
     this.productService.getProducts().subscribe((products: Product[]) => {
       this.products = products;
     });
+  }
+
+  updateProducts(products: Product[]) {
+    this.products = products;
+    console.log('Evento products emitido com: ', this.products);
   }
 }
